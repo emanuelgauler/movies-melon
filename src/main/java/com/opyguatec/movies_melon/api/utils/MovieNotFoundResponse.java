7c7eba03f0eb5bea5b9fb3a476b03812c93a8ce6
emanuelgauler <emanuelgauler@gmail.com>
2024-07-03 20:40:12 -0300

    Se agregó un modelo de respuesta para película no encontrada

---
+++ src/main/java/com/opyguatec/movies_melon/api/utils/MovieNotFoundResponse.java
@@ -0,0 +1,7 @@
+package com.opyguatec.movies_melon.api.utils;
+
+public class MovieNotFoundResponse {
+   public String getMessage() {
+      return "No se encontró la película solicitada";
+   }
+}
