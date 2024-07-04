a1bb621cb08a82c2d1eced3e7731080d65cb998b
emanuelgauler <emanuelgauler@gmail.com>
2024-07-04 12:19:02 -0300

    se agregó la función para eliminar una película

--- src/main/java/com/opyguatec/movies_melon/api/MovieServlet.java
+++ src/main/java/com/opyguatec/movies_melon/api/MovieServlet.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opyguatec.movies_melon.api.utils.MovieNotFoundResponse;
 import com.opyguatec.movies_melon.api.utils.MovieResponseModel;
 import com.opyguatec.movies_melon.core.Movie;
+import com.opyguatec.movies_melon.core.MovieNotFoundError;
 import com.opyguatec.movies_melon.core.MoviesMelon;
 import com.opyguatec.movies_melon.data.StoreInMemory;
 
@@ -24,8 +25,14 @@ public class MovieServlet extends HttpServlet {
 
    @Override
    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      // TODO Auto-generated method stub
-      super.doDelete(req, resp);
+      try {
+         melones.remove_from_id(movie_id_from(req));
+      } catch (MovieNotFoundError e) {
+         resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+         resp.getWriter().println(mapper.writeValueAsString(new MovieNotFoundResponse()));
+      } catch (Exception e) {
+         resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+      }
    }
 
    @Override
