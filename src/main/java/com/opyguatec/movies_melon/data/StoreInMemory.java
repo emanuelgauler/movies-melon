fda97298e3076b1890cbe4f1c80be782476dcfc7
emanuelgauler <emanuelgauler@gmail.com>
2024-07-05 16:09:00 -0300

    se cambió la construcción de las películas ingresadas al almacén

--- src/main/java/com/opyguatec/movies_melon/data/StoreInMemory.java
+++ src/main/java/com/opyguatec/movies_melon/data/StoreInMemory.java
@@ -1,10 +1,11 @@
 package com.opyguatec.movies_melon.data;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import com.opyguatec.movies_melon.core.Movie;
 import com.opyguatec.movies_melon.core.MovieExistingError;
@@ -17,17 +18,23 @@ public class StoreInMemory implements MoviesStore {
    
    public StoreInMemory() throws ParseException {
       if (movies.isEmpty()) {
-         DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+         DateFormat parser = new SimpleDateFormat("dd/MM/yyyy");
          movies.add(
                Movie.with("Furiosa: La Saga de Mad Max",
                      "En un mundo postapocalíptico donde todo ha perdido su valor, los pocos supervivientes se guían por la ley del más fuerte. Sin aprecio por la vida, lo único que despierta un brutal interés es la gasolina, sinónimo de poder y objetivo de bandas armadas hasta los dientes y sin escrúpulos. En este contexto conoceremos la historia de la arrebatadoramente despiadada, salvaje y joven Furiosa, quien cae en manos de una horda de motoristas y debe sobrevivir a muchas pruebas mientras reúne los medios para encontrar el camino de vuelta a casa. Precuela de 'Mad Max: Furia en la carretera' (2015).",
-                     formatter.parse("23/05/2024"),
-                     "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tGHUlykWn9V2IIQ4ZaATIAq9VLB.jpg"));
+                     parser.parse("23/05/2024"),
+                     "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tGHUlykWn9V2IIQ4ZaATIAq9VLB.jpg",
+                     "George Miller",
+                     new ArrayList<>(List.of("Anya Taylor-Joy", "Chris Hemsworth", "Tom Burke", "Alyla Browne",
+                           "George Shevtsov", "Lachy Hulme"))));
          movies.add(
                Movie.with("Atlas",
                      "Una brillante analista antiterrorista recela de la IA hasta que descubre que podría ser su única esperanza cuando se tuerce una misión para atrapar a un robot rebelde.",
-                     formatter.parse("24/5/2024"),
-                     "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wSnBSv7oHgm1kZmiM8IqithlTmJ.jpg"));
+                     parser.parse("24/5/2024"),
+                     "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wSnBSv7oHgm1kZmiM8IqithlTmJ.jpg"
+                     , "Brad Peyton"
+                     , new ArrayList<>(List.of("Jennifer Lopez", "Simu Liu", "Sterling K. Brown"
+                     , "Gregory James Cohan","Abraham Popoola", "Lana Parrilla"))));
 
       }
    }
